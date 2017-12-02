11ecfa9026a2e2ba2624d1cda20d6479f6f62bd5
flacode <flavianshemerirwe@gmail.com>
2017-12-02 08:32:16 +0300

    [Chore #153114587] Add more tests to test check_token view

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from hc.test import BaseTestCase
+from django.urls import reverse
 
 
 class CheckTokenTestCase(BaseTestCase):
@@ -8,13 +9,14 @@ class CheckTokenTestCase(BaseTestCase):
         super(CheckTokenTestCase, self).setUp()
         self.profile.token = make_password("secret-token")
         self.profile.save()
+        self.url = reverse('hc-check-token', args=('alice', 'secret-token') )
 
     def test_it_shows_form(self):
-        r = self.client.get("/accounts/check_token/alice/secret-token/")
+        r = self.client.get(self.url)
         self.assertContains(r, "You are about to log in")
 
     def test_it_redirects(self):
-        r = self.client.post("/accounts/check_token/alice/secret-token/")
+        r = self.client.post(self.url)
         self.assertRedirects(r, "/checks/")
 
         # After login, token should be blank
@@ -22,7 +24,20 @@ class CheckTokenTestCase(BaseTestCase):
         self.assertEqual(self.profile.token, "")
 
     ### Login and test it redirects already logged in
+    def test_login_redirects_to_checks(self):
+        # before login
+        response = self.client.get(self.url)
+        self.assertFalse(response.status_code == 302)
+        self.assertTemplateUsed('accounts/check_token_submit.html')
+        # after login
+        self.client.post(self.url, follow=True)
+        r = self.client.get(self.url)
+        self.assertRedirects(r, reverse('hc-checks'))
+        self.assertTrue(r.status_code == 302)
 
     ### Login with a bad token and check that it redirects
+    def test_bad_token_redirects(self):
+        r = self.client.post(reverse('hc-check-token', args=('alice', 'bad_token')), follow=True)
+        self.assertRedirects(r, reverse('hc-login'))
 
     ### Any other tests?
