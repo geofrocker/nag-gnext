b4d9c6fdc8e96ab3dba6545afde2311bc6041bb2
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-12-04 16:03:50 +0300

    [Chore #153114587]Add docstrings

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -5,8 +5,8 @@ from hc.api.models import Check
 from django.urls import reverse
 from hc.accounts.forms import EmailPasswordForm
 
-class LoginTestCase(TestCase):
 
+class LoginTestCase(TestCase):
     def setUp(self):
         self.url = reverse('hc-login')
         self.check = Check()
@@ -14,6 +14,9 @@ class LoginTestCase(TestCase):
         self.wrong_credentials = {"email": "wrong@email.com", "password": "wrongpassword"}
         
     def test_it_sends_link(self):
+        """
+        Test login link is sent after login.
+        """
         self.check.save()
         session = self.client.session
         session["welcome_code"] = str(self.check.code)
@@ -31,7 +34,7 @@ class LoginTestCase(TestCase):
 
         ### Assert contents of the email body
         created_user = User.objects.first()
-        self.assertTrue(len(mail.outbox[0].body) > 0)
+        self.assertGreater(len(mail.outbox[0].body), 0)
         self.assertIn(created_user.username, mail.outbox[0].body)
 
         ### Assert that check is associated with the new user
@@ -39,21 +42,37 @@ class LoginTestCase(TestCase):
         self.assertEqual(check_user.username, created_user.username)
 
     def test_it_pops_bad_link_from_session(self):
+        """
+        Test bad_link flag is cleared from the session on GET requests.
+        """
+
         self.client.session["bad_link"] = True
         self.client.get(self.url)
         assert "bad_link" not in self.client.session
 
         ### Any other tests?
     def test_login_returns_form_for_get(self):
+        """
+        Test rendered form is correct.
+        """
+
         r = self.client.get(self.url)
         self.assertTemplateUsed('accounts/login.html')
         self.assertEqual(EmailPasswordForm, r.context['form'].__class__)
 
     def test_redirect_with_successful_login(self):
+        """
+        Test user is redirected to checks page on successful login.
+        """
+
         r = self.client.post(self.url, self.form, follow=True)
         self.assertRedirects(r, reverse('hc-login-link-sent'))
 
     def test_login_with_incorrect_password(self):
+        """
+        Test error message is rendered when user supplies incorrect login credentials.
+        """
+
         r = self.client.post(self.url, self.wrong_credentials)
         # bad_credentials should be True in the context
         self.assertTrue(r.context['bad_credentials'] is True)
@@ -64,6 +83,10 @@ class LoginTestCase(TestCase):
             User.objects.get(email=self.wrong_credentials["email"])
 
     def test_login_link_cannot_be_used_twice(self):
+        """
+        Test user should use login link only once.
+        """
+
         r = self.client.post(self.url, self.form)
         login_link = r.context['login_link']
 
--- hc/accounts/tests/test_profile.py
+++ hc/accounts/tests/test_profile.py
@@ -9,6 +9,10 @@ from hc.api.models import Check
 class ProfileTestCase(BaseTestCase):
 
     def test_it_sends_set_password_link(self):
+        """
+        Test set password link is sent to user email.
+        """
+
         self.client.login(username="alice@example.org", password="password")
 
         form = {"set_password": "1"}
@@ -18,9 +22,10 @@ class ProfileTestCase(BaseTestCase):
         # profile.token should be set now
         self.alice.profile.refresh_from_db()
         token = self.alice.profile.token
+
         ### Assert that the token is set
         self.assertIsNotNone(token)
-        self.assertTrue(len(token) > 0)
+        self.assertGreater(len(token), 0)
 
         ### Assert that the email was sent and check email content
         self.assertIn('set_password_link', r.context)
@@ -28,23 +33,31 @@ class ProfileTestCase(BaseTestCase):
         link = r.context['set_password_link']
         outbox = mail.outbox
 
-        self.assertTrue(len(outbox) > 0)
+        self.assertGreater(len(outbox), 0)
         self.assertIn(link, outbox[0].body)
 
     def test_it_sends_report(self):
+        """
+        Test check report is sent user via email.
+        """
+
         check = Check(name="Test Check", user=self.alice)
         check.save()
 
         self.alice.profile.send_report()
-        ###Assert that the email was sent and check email content
 
+        ### Assert that the email was sent and check email content
         outbox = mail.outbox
 
-        self.assertTrue(len(outbox) > 0)
+        self.assertGreater(len(outbox), 0)
         self.assertIsNot(outbox[0].body, "")  # should not be empty.
         self.assertIn('This is a monthly report sent by healthchecks.io.', outbox[0].body)
 
     def test_it_adds_team_member(self):
+        """
+        Test user is added to created team via an invite.
+        """
+
         self.client.login(username="alice@example.org", password="password")
 
         form = {"invite_team_member": "1", "email": "frank@example.org"}
@@ -72,6 +85,10 @@ class ProfileTestCase(BaseTestCase):
         self.assertIn(self.alice.email, outbox[0].body)
 
     def test_add_team_member_checks_team_access_allowed_flag(self):
+        """
+        Test users with team access privileges can invite team members.
+        """
+
         self.client.login(username="charlie@example.org", password="password")
 
         form = {"invite_team_member": "1", "email": "frank@example.org"}
@@ -79,6 +96,10 @@ class ProfileTestCase(BaseTestCase):
         assert r.status_code == 403
 
     def test_it_removes_team_member(self):
+        """
+        Test team owners can remove team members.
+        """
+
         self.client.login(username="alice@example.org", password="password")
 
         form = {"remove_team_member": "1", "email": "bob@example.org"}
@@ -91,6 +112,10 @@ class ProfileTestCase(BaseTestCase):
         self.assertEqual(self.bobs_profile.current_team, None)
 
     def test_it_sets_team_name(self):
+        """
+        Test team owner can set team name.
+        """
+
         self.client.login(username="alice@example.org", password="password")
 
         form = {"set_team_name": "1", "team_name": "Alpha Team"}
@@ -101,6 +126,10 @@ class ProfileTestCase(BaseTestCase):
         self.assertEqual(self.alice.profile.team_name, "Alpha Team")
 
     def test_set_team_name_checks_team_access_allowed_flag(self):
+        """
+        Test only team members can set team name.
+        """
+
         self.client.login(username="charlie@example.org", password="password")
 
         form = {"set_team_name": "1", "team_name": "Charlies Team"}
@@ -108,10 +137,12 @@ class ProfileTestCase(BaseTestCase):
         assert r.status_code == 403
 
     def test_it_switches_to_own_team(self):
+        """
+        Test
+        """
         self.client.login(username="bob@example.org", password="password")
 
         self.client.get("/accounts/profile/")
-
         # After visiting the profile page, team should be switched back
         # to user's default team.
         self.bobs_profile.refresh_from_db()
@@ -133,7 +164,7 @@ class ProfileTestCase(BaseTestCase):
         self.assertNotContains(r, "bobs-tag.svg")
 
     ### Test it creates and revokes API key
-    def test_it_creates_and_revoke_api(self):
+    def test_it_creates_and_revoke_api(self):  # name
         # login charlie.
         self.client.login(username=self.charlie.email, password='password')
         form = {'create_api_key': "1"}
@@ -156,4 +187,4 @@ class ProfileTestCase(BaseTestCase):
         self.charlie.profile.refresh_from_db()
 
         self.assertContains(response, 'The API key has been revoked!')
-        self.assertTrue(self.charlie.profile.api_key == "")
+        self.assertTrue(self.charlie.profile.api_key == "")  # name.
