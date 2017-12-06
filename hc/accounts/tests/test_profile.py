57a4564976b9a9e43e5317831b6d231ab550a082
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-12-06 12:48:58 +0300

    [Chore #153114587]Fix assertion statements and remove duplicate lines of code

--- hc/accounts/tests/test_profile.py
+++ hc/accounts/tests/test_profile.py
@@ -7,13 +7,14 @@ from hc.api.models import Check
 
 
 class ProfileTestCase(BaseTestCase):
+    def setUp(self):
+        super(ProfileTestCase, self).setUp()
+        self.client.login(username="alice@example.org", password="password")
 
     def test_set_password_link_is_sent(self):
         """
         Test set password link is sent to user email.
         """
-
-        self.client.login(username="alice@example.org", password="password")
 
         form = {"set_password": "1"}
         r = self.client.post("/accounts/profile/", form)
@@ -23,17 +24,32 @@ class ProfileTestCase(BaseTestCase):
         self.alice.profile.refresh_from_db()
         token = self.alice.profile.token
 
-        # Todo Assert that the token is set
+        # Assert that the token is set
         self.assertIsNotNone(token)
         self.assertGreater(len(token), 0)
 
-        # Todo Assert that the email was sent and check email content
-        self.assertIn('set_password_link', r.context)
+        # Assert that the email was sent and check email content
 
+        # expected subject.
+        subject = 'Set password on healthchecks.io'
+
+        # expected body content.
+        body = "Here's a link to set a password for your account on healthchecks.io"
+
+        # get set password link from context.
         link = r.context['set_password_link']
+
         outbox = mail.outbox
 
         self.assertGreater(len(outbox), 0)
+
+        # assert subject.
+        self.assertIsNot(outbox[0].subject, "")
+        self.assertEqual(subject, outbox[0].subject)
+
+        # assert body.
+        self.assertIsNot(outbox[0].body, "")
+        self.assertIn(body, outbox[0].body)
         self.assertIn(link, outbox[0].body)
 
     def test_report_is_sent(self):
@@ -46,7 +62,7 @@ class ProfileTestCase(BaseTestCase):
 
         self.alice.profile.send_report()
 
-        # Todo Assert that the email was sent and check email content
+        # Assert that the email was sent and check email content
         outbox = mail.outbox
 
         self.assertGreater(len(outbox), 0)
@@ -60,8 +76,6 @@ class ProfileTestCase(BaseTestCase):
         Test user is added to created team via an invite.
         """
 
-        self.client.login(username="alice@example.org", password="password")
-
         form = {"invite_team_member": "1", "email": "frank@example.org"}
         r = self.client.post("/accounts/profile/", form)
         assert r.status_code == 200
@@ -70,12 +84,12 @@ class ProfileTestCase(BaseTestCase):
         for member in self.alice.profile.member_set.all():
             member_emails.add(member.user.email)
 
-        # Todo Assert the existence of the member emails
+        # Assert the existence of the member emails
         self.assertGreater(len(member_emails), 0)
 
         self.assertTrue("frank@example.org" in member_emails)
 
-        # Todo Assert that the email was sent and check email content
+        # Assert that the email was sent and check email content
         # expected subject message.
         subject = 'You have been invited to join ' \
                   '%(email)s on healthchecks.io' % dict(email=self.alice.email)
@@ -102,8 +116,6 @@ class ProfileTestCase(BaseTestCase):
         Test team owners can remove team members.
         """
 
-        self.client.login(username="alice@example.org", password="password")
-
         form = {"remove_team_member": "1", "email": "bob@example.org"}
         r = self.client.post("/accounts/profile/", form)
         assert r.status_code == 200
@@ -117,8 +129,6 @@ class ProfileTestCase(BaseTestCase):
         """
         Test team owner can set team name.
         """
-
-        self.client.login(username="alice@example.org", password="password")
 
         form = {"set_team_name": "1", "team_name": "Alpha Team"}
         r = self.client.post("/accounts/profile/", form)
@@ -156,7 +166,6 @@ class ProfileTestCase(BaseTestCase):
         Test users should be able to see badges.
         """
 
-        self.client.login(username="alice@example.org", password="password")
         Check.objects.create(user=self.alice, tags="foo a-B_1  baz@")
         Check.objects.create(user=self.bob, tags="bobs-tag")
 
@@ -170,14 +179,20 @@ class ProfileTestCase(BaseTestCase):
         # Expect only Alice's tags
         self.assertNotContains(r, "bobs-tag.svg")
 
-    # Todo Test it creates and revokes API key
-    def test_user_can_create_and_revoke_api(self):
+
+class ApiKeyCase(BaseTestCase):
+    def setUp(self):
+        super(ApiKeyCase, self).setUp()
+
+        # login Charlie.
+        self.client.login(username=self.charlie.email, password="password")
+
+    # Test it creates and revokes API key
+    def test_user_can_create_api(self):
         """
         Test user should be able to create and revoke API keys.
         """
 
-        # login charlie.
-        self.client.login(username=self.charlie.email, password='password')
         form = {'create_api_key': "1"}
 
         response = self.client.post(reverse('hc-profile'), form)
@@ -188,9 +203,10 @@ class ProfileTestCase(BaseTestCase):
 
         self.charlie.profile.refresh_from_db()
 
-        # # check API key now actually exists in Charlie profile.
+        # API key now actually exists in Charlie profile.
         self.assertIsNot(self.charlie.profile.api_key, "")
 
+    def test_user_can_revoke_api(self):
         # revoke API key.
         form = {'revoke_api_key': "1"}
         response = self.client.post(reverse('hc-profile'), form)
@@ -198,4 +214,19 @@ class ProfileTestCase(BaseTestCase):
         self.charlie.profile.refresh_from_db()
 
         self.assertContains(response, 'The API key has been revoked!')
-        self.assertTrue(self.charlie.profile.api_key == "")  # name.
+
+        # API key should be empty.
+        self.assertEqual(self.charlie.profile.api_key, "")
+
+"""
+Completed. 
+
+1. Email contents not checked.
+2. Dry (self.client.login(username="alice@example.org", password="password").
+3. Make a separate tests. (No need since it involves testing an inbuilt feature).
+4. Separated tests create and revoke api key.
+5. Fixed assert statement(assertTrue and assertEqual).
+6. Assert contents of the response.
+7. 
+
+"""
